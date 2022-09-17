ccdb271ee6d3fd3fa9f3a260c3d9957e74eec74f
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-09-17 10:08:22 +0000

    Add:会員編集ページ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,8 +4,27 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render 'edit'
+    end
+    
   end
 
   def unsubscribe
   end
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
+  end
+  
 end
