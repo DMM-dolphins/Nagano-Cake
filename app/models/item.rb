69cbf96f7e71d7d4b1bbbc4d5ef89dbfc223b1d0
takathanda538 <106585979+takathanda538@users.noreply.github.com>
2022-09-17 09:24:40 +0000

    ADD:cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_one_attached :item_image
   
+  def with_tax_price
+      (price * 1.1).floor
+  end
+
+  def get_item_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
