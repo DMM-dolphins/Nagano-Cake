f4e566aed6b58a24e1adce0a0ee71a7d8ddc492f
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-09-17 05:59:20 +0000

    Add:配送先登録機能

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,31 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+  
+  def create
+    @address = Address.new(addresses_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+  
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+  
+  
+  private
+  
+  def addresses_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+  
 end
