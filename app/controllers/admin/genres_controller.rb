09c8565fbee597b4882a8cf0602b5835bf8407ba
sakatashi <shiori.1002sakata@gmail.com>
2022-09-18 10:45:05 +0000

    Add:genres/edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,5 +10,17 @@ class Admin::GenresController < ApplicationController
     redirect_to admin_genres_path
   end
   def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path(@genre.id)
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
