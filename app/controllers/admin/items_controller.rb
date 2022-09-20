00e254338a22efb76b02eee22352ccfbafb1f559
sakatashi <shiori.1002sakata@gmail.com>
2022-09-20 05:31:39 +0000

    Fix:items/genreコントローラ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ItemsController < ApplicationController
+    before_action :authenticate_admin!
   def index
     @items = Item.all
   end
@@ -11,7 +12,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_items_path
+    redirect_to admin_items_path(@item)
   end
 
 
