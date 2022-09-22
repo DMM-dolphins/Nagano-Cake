434a25e7c8b33ac11a55a79562650c67d756f99a
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-09-22 04:04:02 +0000

    Fix:ログイン後の遷移画面と注文確認画面の修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :customer_state, only: [:create]
+
+  def after_sign_in_path_for(resource)
+    customer_path(current_customer.id)
+  end
   
   protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
